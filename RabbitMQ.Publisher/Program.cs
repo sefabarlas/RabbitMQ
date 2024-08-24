8d4fe5b929b9e5018768d4d7f9f3ce8c20e9ba16
Sefa Barlas <sefabarlass@gmail.com>
2024-08-24 15:13:50 +0300

    work queue

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://localhost:5672")
+};
+
+using var connection = connectionFactory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    var message = $"Message {x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Mesaj Gönderildi: {message}");
+});
+
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://localhost:5672")
+};
+
+using var connection = connectionFactory.CreateConnection();
+
+var channel = connection.CreateModel();
+// channel.QueueDeclare("hello-queue", true, false, false);
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: " + message);
+    channel.BasicAck(eventArgs.DeliveryTag, false);
+};
+
+Console.ReadLine();
